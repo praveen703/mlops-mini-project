0e393c89fabae1cd85875770fef4725ea14b7fd9
praveen <tyagiboypraveen97@gmail.com>
2025-07-28 22:31:36 +0530

    add experiment 1

---
+++ notebooks/dagshub_setup.py
@@ -0,0 +1,9 @@
+import dagshub
+import mlflow
+
+mlflow.set_tracking_uri('https://dagshub.com/praveen703/mlops-mini-project.mlflow')
+dagshub.init(repo_owner='praveen703', repo_name='mlops-mini-project', mlflow=True)
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
